800a6f606130cdb2fe0b0aabdd7fa333681771dc
Intezium <Intezium04@gmail.com>
2018-03-17 16:23:28 +0300

    Добавлена Binary и Xml сериализация

--- BinaryAndXmlSerialization/Program.cs
+++ BinaryAndXmlSerialization/Program.cs
@@ -1,15 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+using SerializationDLL;
 
 namespace BinaryAndXmlSerialization
 {
+    //Binary serialization
+    [Serializable]
+    public class Car
+    {
+        public int model;
+
+        public Car()
+        {
+            model = 1;
+        }
+
+        public override string ToString()
+        {
+            return model.ToString();
+        }
+    }
+
+    //Xml serialization
+    public class Person
+    {
+        [XmlAttribute]
+        public string name;
+
+        [XmlAttribute]
+        public int age;
+
+        public Person()
+        {
+            name = "Tod";
+            age = 29;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", name, age);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            //Binary serialization
+            string pathCar = "D://Car_SerializationFile.txt";
+
+            Car car = new Car();
+            Car car2 = null;
+
+            Binary_Serializer.Serialize(pathCar, car);
+            Binary_Serializer.Deserialize(pathCar, ref car2);
+
+            Console.WriteLine("[Car]\nModel: {0}\n", car2.model);
+            //===
+
+
+            //Xml serialization
+            string pathPerson = "D://Person_SerializationFile.xml";
+
+            Person person = new Person();
+            Person person2 = null;
+
+            Xml_Serializer.Serialize(pathPerson, person);
+            Xml_Serializer.Deserialize(pathPerson, ref person2);
+
+            Console.WriteLine("[Person]\nName: {0}\nAge: {1}", person2.name, person2.age);
+            //===
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SerializationDLL/SerializationDLL.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace SerializationDLL
+{
+    public static class Binary_Serializer
+    {
+        static BinaryFormatter serializer;
+
+        public static void Serialize<T>(string path, T obj) where T : new()
+        {
+            serializer = new BinaryFormatter();
+
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                serializer.Serialize(fileStream, obj);
+            }
+        }
+
+        public static void Deserialize<T>(string path, ref T obj) where T : new()
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException();
+            else
+            {
+                serializer = new BinaryFormatter();
+
+                using (FileStream fileStream = new FileStream(path, FileMode.Open))
+                {
+                    obj = (T)serializer.Deserialize(fileStream);
+                }
+            }
+        }
+    }
+
+    public static class Xml_Serializer
+    {
+        static XmlSerializer serializer;
+
+        public static void Serialize<T>(string path, T obj) where T : new()
+        {
+            serializer = new XmlSerializer(typeof(T));
+
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                serializer.Serialize(fs, obj);
+            }
+        }
+
+        public static void Deserialize<T>(string path, ref T obj) where T : new()
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException();
+            else
+            {
+                serializer = new XmlSerializer(typeof(T));
+
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    obj = (T)serializer.Deserialize(fs);
+                }
+            }
+        }
+    }
+}
